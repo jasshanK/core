ad3cd0661185038bc11bd85101c7d0150df3f7a9
jasshanK <jasshank@gmail.com>
2024-10-26 10:35:07 +0800

    update

--- hosts/desktop/syncthing.nix
+++ hosts/desktop/syncthing.nix
@@ -10,8 +10,15 @@
           "laptop" = { 
             id = "KMWIVTC-G5CMA6D-QITJ7BD-XI7UK63-L2BPQHR-5QY3H2T-V2SBCGN-5ROORAO";
           };
+          "mobile" = { 
+            id = "KYLZSPC-TMXWICL-E5LLABP-IN7KQQY-LAACVAZ-FHIRDCJ-45V3J57-2QAMGA5";
+          };
         };
         folders = {
+          "Notes" = {
+            path = "/home/jasshank/notes";
+            devices = [ "laptop" "mobile" ];
+          };
           "Projects" = {
             path = "/home/jasshank/proj";
             devices = [ "laptop" ];
