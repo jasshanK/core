97fc2cd597c02af3e158b938cd53e48d4c6390bd
jasshanK <jasshank@gmail.com>
2025-02-13 21:12:49 +0800

    updated web search script

--- src/scripts/web.sh
+++ src/scripts/web.sh
@@ -7,10 +7,18 @@ white="#abb2bf"
 function web {
     selection=$(dmenu -fn $font -nb $black -nf $white -sb $white -sf $black < /dev/null)
 
-    # ${parameter/pattern/string}
-    # parameter is the variable
-    # if pattern begins with /, all matches of the pattern are replaced
-    firefox "https://www.google.com/search?q="${selection// /+}
+    if [ -z "${selection}" ]; then
+        return 1
+    fi
+    
+    if grep -q "." <<< "${selection}"; then
+        firefox "${selection}"
+    else
+        # ${parameter/pattern/string}
+        # parameter is the variable
+        # if pattern begins with /, all matches of the pattern are replaced
+        firefox "https://www.google.com/search?q="${selection// /+}
+    fi
 }
 
 web
