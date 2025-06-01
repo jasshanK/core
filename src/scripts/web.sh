0b8258a46d8c678cd55ed8a082899098da8e65f1
jasshanK <jasshank@gmail.com>
2025-06-01 22:34:36 +0800

    fixed web search script

--- src/scripts/web.sh
+++ src/scripts/web.sh
@@ -5,20 +5,23 @@ black="#000000"
 white="#abb2bf"
 
 function web {
-    selection=$(dmenu -fn $font -nb $black -nf $white -sb $white -sf $black < /dev/null)
+    selection=$(dmenu -fn $font -nb $black -nf $white -sb $white -sf $black -p "search/url"< /dev/null)
 
     if [ -z "${selection}" ]; then
         return 1
     fi
-    
-    if grep -q "." <<< "${selection}"; then
-        firefox "${selection}"
-    else
-        # ${parameter/pattern/string}
-        # parameter is the variable
-        # if pattern begins with /, all matches of the pattern are replaced
-        firefox "https://www.google.com/search?q="${selection// /+}
-    fi
+
+    case "${selection}" in
+        *"."*)
+            firefox "${selection}"
+            ;;
+        *)
+            # ${parameter/pattern/string}
+            # parameter is the variable
+            # if pattern begins with /, all matches of the pattern are replaced
+            firefox "https://www.google.com/search?q="${selection// /+}
+            ;;
+    esac
 }
 
 web
